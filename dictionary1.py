83bb967af8dbbc93e12de1f846edbded240b92d8
subhamkhd <pandasubhamkumar111@gmail.com>
2021-04-23 10:48:57 +0530

    Future Skills Submission

---
+++ dictionary1.py
@@ -0,0 +1,22 @@
+""""
+Write a Python script to merge two Python dictionaries.
+"""
+def dict_merge(dict1,dict2):
+    merge=dict1 | dict2
+    return merge
+
+dict1={"Name":"Subh", "Age":22}
+dict2={"height":5.8, "weight":"68kg"}
+dm= dict_merge(dict1,dict2)   # storing the output of the dict_merge in the variable dm
+print(dm)
+
+#this code is contributed by Subham Kumar Panda
+
+"""Write a Python program to convert a list into a nested dictionary of keys"""
+
+num_list = [1, 2, 3, 4]
+new_dict = current = {}
+for ele in num_list:
+    current[ele] = {}
+    current = current[ele]
+print(new_dict)
